30198dcc0230f165dc362cc21cd4d005d535837a
jipazmino <jpazminomino@cca.edu>
2017-02-22 12:30:25 -0800

    Update LeWitt-335.js

--- LeWitt-335.js
+++ LeWitt-335.js
@@ -1,1 +1,27 @@
 
+  
+  function setup() { 
+  createCanvas(600, 400);
+} 
+
+
+
+function draw() { 
+  background(0);
+
+var z = 0
+
+  
+for (x=0;x<=600;x=x+5){
+  stroke(255);
+    line (z,x,width,x);
+
+}
+  ellipseMode (CENTER);
+
+  fill(0);
+
+  ellipse(300,200,200,200);{
+    
+}
+}
